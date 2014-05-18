ce38eb4e0d52da73b31075ea03e395a77de1d2b6
Jason Nadro <jason.nadro@gmail.com>
2014-05-18 12:06:34 -0500

    fix paul not showing up
    screw you javascript

--- Paul/js/paul.js
+++ Paul/js/paul.js
@@ -3,10 +3,10 @@ var game = game || {};
 game.Paul = function() {
 	this.texture = PIXI.Texture.fromImage("sprites/flannel.png");
 	this.sprite = new PIXI.Sprite(this.texture);
-	this.sprite.anchor = 0.5;
-	this.sprite.anchor = 0.5;
+	this.sprite.anchor.x = 0.5;
+	this.sprite.anchor.y = 0.5;
 	this.sprite.position.x = game.renderer.width / 2.0;
-	this.sprite.position.y = game.renderer.width / 2.0;
+	this.sprite.position.y = game.renderer.height / 2.0;
 
 	// Add to stage
 	game.stage.addChild(this.sprite);
