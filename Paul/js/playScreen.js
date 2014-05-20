3903ff8990b29a55d5a1831dfe21236bfeed3098
Kevin Bacon <khomer8@yahoo.com>
2014-05-19 20:05:40 -0500

    lumber mill is added
    - I added the lumbermill.js and the images for it
    
    -The lumber mill is animated, again use the core to edit its position if
    needed. Also some of the parts are added to game.stage and others are
    added to the core, all of the parts are relative to the core
    
    - if you need to delete the mill, call the destroy(); function that it
    has because it will stop all the tweens and delete each sprite whether
    it was added to the core or game stage

--- Paul/js/playScreen.js
+++ Paul/js/playScreen.js
@@ -29,12 +29,14 @@ game.PlayScreen = function() {
 		this.paul = new game.Paul();
 		this.tree = new game.Tree({ x: 150, y: 100 }, 24);
 		this.wolf = new game.Wolf({ x: 250, y: 250});
+		this.lumbermill = new game.LumberMill({x: 150, y:550});
 
 		_.extend(game.Paul.prototype, game.Moving);
 
 		this.entities.push(this.paul);
 		this.entities.push(this.tree);
 		this.entities.push(this.wolf);
+		this.entities.push(this.lumbermill);
 	};
 
 	this.exit = function() {
