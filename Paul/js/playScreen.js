cfe6ceca87950a4741bc21098e0f95b2bb7a5300
Kevin <khomer8@yahoo.com>
2014-05-21 13:57:48 -0500

    added a forest
    -when the playscreen gets created it makes the forest
    -treepositions keeps track of all of the positions of the trees
    maybe useful?
    -This function to create the grid of trees isn't very friendly, it
    needs a lot of tweaking to make sure area's of the trees don't overlap
    or we need to write something that won't let you chop two trees at once
    
    -Also to make the game harder we should just scale up the time it takes
    to chop a tree, get paper,then increase the health of animals

--- Paul/js/playScreen.js
+++ Paul/js/playScreen.js
@@ -8,7 +8,7 @@ game.PlayScreen = function() {
 	var paperText = function() {
 		return 'Paper : ' + self.paper;
 	};
-
+	this.treepositions = [];
 	this.entities = [];
 	this.enemies = [];
 	this.lumber = 0;
@@ -16,6 +16,27 @@ game.PlayScreen = function() {
 
 	this.lumberGUI = new game.TextWidget(lumberText(), game.renderer.width * 0.25, 0.0);
 	this.PaperGUI = new game.TextWidget(paperText(), game.renderer.width * 0.35, 0.0);
+
+	this.createtreepositions = function(){
+		var position = {x:null,y:null}	
+		var offsetX = -50;
+		var offsetY = -75;
+		var rows = 3;
+		var columns = 8;
+		// Creates 3 rows
+		for (var w = 1; w < (rows+1); w++) {
+			position.y = (w*200) + offsetY;
+			for (var i = 1; i < (columns+1); i++) {
+				position.x = (i*150) + offsetX;
+				this.treepositions.push(position);
+				var tree = new game.Tree({x:position.x, y:position.y}, 24);
+				this.entities.push(tree);
+			};
+		};
+	};
+	
+	// Fills the tree position array with the spawn locations of the trees
+	this.createtreepositions();
 
 	this.getLumberGUIPosition = function() {
 		return { x: this.lumberGUI.sprite.position.x, y: this.lumberGUI.sprite.position.y };
@@ -46,13 +67,13 @@ game.PlayScreen = function() {
 	};
 
 	this.enter = function() {
-		this.lumbermill = new game.LumberMill({x: 150, y:550});
+		this.lumbermill = new game.LumberMill({x: 100, y:650});
 		this.paul = new game.Paul();
-		this.tree = new game.Tree({ x: 500, y: 500 }, 24);
+		//this.tree = new game.Tree({ x: 500, y: 500 }, 24);
 		
 		this.entities.push(this.lumbermill);
 		this.entities.push(this.paul);
-		this.entities.push(this.tree);
+		//this.entities.push(this.tree);
 
 		var self = this;
 		this.wolfSpawner = new game.Spawner(game.Wolf, 1, 1000, { x: 250, y: 250 }, function(w) {
