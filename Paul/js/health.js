4cfd815045a33193ae6a1949e152ef4eeebd5410
Kevin Bacon <khomer8@yahoo.com>
2014-05-21 12:00:57 -0500

    wolf dies
    -coded the ondeath function, so when the wolf dies he will be removed
    from the enemies array and the game.stage

--- Paul/js/health.js
+++ Paul/js/health.js
@@ -7,11 +7,13 @@ game.HP = {
 	takeDamage: function(dmg, entity) {
 		this.hp -= dmg;
 		this.core.tint = 0x7F0000;
-		if (this.hp < 0) {
-			this.onDeath();
+		if (this.hp <= 0) {
+			this.onDeath(entity);
 		}
 	},
-	onDeath: function() {
-		
+	onDeath: function(entity) {
+		game.stage.removeChild(entity.core);
+		var i = game.state.currentScreen.enemies.indexOf(entity);
+		game.state.currentScreen.enemies.splice(i,1);	
 	}
 };
