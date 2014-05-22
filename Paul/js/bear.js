8c7a0f12f477e33cd4975231603d89915c0bde1b
Jason Nadro <jason.nadro@gmail.com>
2014-05-22 09:07:53 -0500

    give bear same functionality as wolf
    -lollers copy/paste from the wolf.  this is bad mmkay.

--- Paul/js/bear.js
+++ Paul/js/bear.js
@@ -215,11 +215,26 @@ game.Bear = function(position) {
 
 		this.facepaul(screen);
 
+		// are we near paul?
+		var near = screen.nearPaul(this);
+
 		if (this.currentState === this.states.hunting) {
 			this.force = this.seek(screen.paul);
+
+			// if we are near paul chang state to attacking.
+			if (near) {
+				this.currentState = this.states.attacking;
+			}
 		}
 		else if (this.currentState === this.states.attacking) {
+			// stop the bear
+			this.force = { x: 0.0, y: 0.0 };
+			this.velocity = { x: 0.0, y: 0.0 };
 
+			// if we are not near paul anymore change state
+			if (!near) {
+				this.currentState = this.states.hunting;
+			}
 		}
 
 		this.updateSteering(delta);
